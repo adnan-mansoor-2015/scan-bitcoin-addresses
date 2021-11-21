9acf7609a564891ae450cb7fc9d8350bc6b387dc
Adnan Mansoor <adnan.mansoor@prefectos.com>
2021-11-21 18:43:23 +0500

    More test cases added for files scan directly

--- main_test.go
+++ main_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Testing Possible Matches
-func TestPerfectMatches(t *testing.T) {
+// Testing Possible Matches Directory
+func TestPerfectMatchesDirectory(t *testing.T) {
 	matchDetails, err := cmd.ScanDirectory("testDirectory/perfect")
 	require.NoError(t, err, "Failed to scan directory")
 	for _, matchDetail := range matchDetails {
@@ -19,8 +19,8 @@ func TestPerfectMatches(t *testing.T) {
 	}
 }
 
-// Testing Perfect Matches
-func TestPossibleMatches(t *testing.T) {
+// Testing Perfect Matches Directory
+func TestPossibleMatchesDirectory(t *testing.T) {
 	matchDetails, err := cmd.ScanDirectory("testDirectory/possible")
 	require.NoError(t, err, "Failed to scan directory")
 	for _, matchDetail := range matchDetails {
@@ -30,13 +30,46 @@ func TestPossibleMatches(t *testing.T) {
 	}
 }
 
-// Testing Possible Matches
-func TestNoMatchesMatches(t *testing.T) {
+// Testing Possible Matches Directory
+func TestNoMatchesMatchesDirectory(t *testing.T) {
 	matchDetails, err := cmd.ScanDirectory("testDirectory/nomatch")
 	require.NoError(t, err, "Failed to scan directory")
 	require.Equal(t, 0, len(matchDetails), "Matches found somehow")
 
 	for _, matchDetail := range matchDetails {
-		fmt.Printf("%+v\n", matchDetail)
+		fmt.Printf("Unexpected Match: %+v\n", matchDetail)
+	}
+}
+
+// Testing Possible Matches File
+func TestPerfectMatchesFile(t *testing.T) {
+	matchDetails, err := cmd.ScanDirectory("testDirectory/perfect/btc/perfect.txt")
+	require.NoError(t, err, "Failed to scan file")
+	for _, matchDetail := range matchDetails {
+		if matchDetail.MatchType != "perfect" {
+			require.Equal(t, "perfect", matchDetail.MatchType, "UnExpected Match: %+v", matchDetail)
+		}
+	}
+}
+
+// Testing Perfect Matches File
+func TestPossibleMatchesFile(t *testing.T) {
+	matchDetails, err := cmd.ScanDirectory("testDirectory/possible/btc/possible.txt")
+	require.NoError(t, err, "Failed to scan file")
+	for _, matchDetail := range matchDetails {
+		if matchDetail.MatchType != "possible" {
+			require.Equal(t, "possible", matchDetail.MatchType, "UnExpected Match: %+v", matchDetail)
+		}
+	}
+}
+
+// Testing Possible Matches File
+func TestNoMatchesMatchesFile(t *testing.T) {
+	matchDetails, err := cmd.ScanDirectory("testDirectory/nomatch/btc/nomatch.txt")
+	require.NoError(t, err, "Failed to scan file")
+	require.Equal(t, 0, len(matchDetails), "Matches found somehow")
+
+	for _, matchDetail := range matchDetails {
+		fmt.Printf("Unexpected Match: %+v\n", matchDetail)
 	}
 }
